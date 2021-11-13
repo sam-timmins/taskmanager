c72ebb7ba278ad1f1a097f80ba2f9e0f6c74b4cf
sam-timmins <sam.timmins1@gmail.com>
2021-11-13 20:46:57 +0000

    Add new task form

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -54,7 +54,7 @@ def add_task():
             task_name=request.form.get("task_name"),
             task_description=request.form.get("task_description"),
             is_urgent=bool(True if request.form.get("is_urgent") else False),
-            due_date=request.form.get("task_date"),
+            due_date=request.form.get("due_date"),
             category_id=request.form.get("category_id")
         )
         db.session.add(task)
